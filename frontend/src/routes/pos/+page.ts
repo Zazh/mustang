ae2c1681c3086b2c3a5887b55322a279552c0c49
Zazh <pobiz@inbox.ru>
2025-03-19 20:31:05 +0500

    Frontend Build production

--- frontend/src/routes/pos/+page.ts
+++ frontend/src/routes/pos/+page.ts
@@ -5,7 +5,7 @@ export const load: PageLoad = async ({ fetch, params }) => {
     const baseUrl = import.meta.env.VITE_BACKEND_API_URL || '';
 
     const { id } = params as { id: string }; // Приводим params к нужному типу
-    const res = await fetch(`${baseUrl}pointsales/`);
+    const res = await fetch(`${baseUrl}/pointsales/`);
 
     if (!res.ok) {
         throw new Error('Failed to fetch product');
